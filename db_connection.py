f2d6e36fa4aa80d3f57defd4f46046ed05f2467f
robAnderson <reanderson89@gmail.com>
2023-03-10 10:30:35 -0800

    refactored the table so it matches the assigment, updated routes to match, created to-do list

--- db_connection.py
+++ db_connection.py
@@ -1,5 +1,6 @@
 # pip install mysql-connector-python
 import mysql.connector
+
 my_db = mysql.connector.connect(host="localhost", user="root", password="password")
 
 my_cursor = my_db.cursor()
@@ -10,29 +11,80 @@ my_cursor.execute("create database user_db;")
 my_cursor.execute("use user_db;")
 
 # create table
-my_cursor.execute("create table user (pid int, name varchar(255));")
+my_cursor.execute(
+    """
+CREATE TABLE `user` (
+  `pid` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `uuid` varchar(56) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `sms` varchar(25) DEFAULT NULL,
+  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastUpdated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+"""
+)
 
+# GET all users
 def select_all():
     # select all users from the table (READ)
-    my_cursor.execute("select * from user;")
+    my_cursor.execute("select username, name, email, sms from user;")
     # the fetchall() or fetchone() is going to fetch the information selected from the above query and store it on the result variable
     result = my_cursor.fetchall()
+    dictResult = []
     for row in result:
-        print(row)
-    return result
+        dictResult.append({
+            "username": row[0],
+            "name": row[1],
+            "email": row[2],
+            "sms": row[3],
+        })
+    return dictResult
 
+# GET one user by email, need to refactor to find by UUID
+def select_one_user(email):
+    my_cursor.execute("select username, name, email, sms from user where email=%s", [email])
+    result = my_cursor.fetchone()
+    listResult = [{
+        "username": result[0],
+        "name": result[1],
+        "email": result[2],
+        "sms": result[3],
+    }]
+    return listResult
+
+# POST a user to the database, need to figure out how to create the UUID. Check for built-in python methods.
 def create_user(args):
-    print("db: ",args)
-    insert_sql = "INSERT INTO user (pid, name) VALUES (%s, %s);"
-    user = [args["pid"], args["name"]]
+    print("db: ", args)
+    insert_sql = "INSERT INTO user (uuid, username, name, email, sms) VALUES (%s, %s, %s, %s, %s);"
+    user = [args["uuid"], args["username"], args["name"], args["email"], args["sms"]]
     my_cursor.execute(insert_sql, user)
     # my_db.commit() is needed to save the changes made to the db
     my_db.commit()
+    user_from_db = select_one_user(args["email"])
+    return user_from_db
+
+
+# TO DO:
+# Figure out how to make the UUID
+# Refactor the select_one_user from finding by email to finding by UUID
+# Create the PUT route to update a user
+# Create the DELETE route to delete a user
 
 
 # insert users into the table (CREATE)
-insert_sql = "INSERT INTO user (pid, name) VALUES (%s, %s);"
-users = [(1, "rob"), (2, "elsa"), (3, "appa"), (4, "goose"), (5, 'stranger danger')]
+insert_sql = """
+INSERT INTO user (uuid, username, name, email, sms)
+VALUES(%s, %s, %s, %s, %s);
+"""
+users = [
+    (1, "reanderson89", "Robert", "reanderson89@gmail.com", "5039279423"),
+    (2, "estauder90", "Elsa", "staudere@gmail.com", "5039613187"),
+    (3, "appaKnowHow", "Appa", "missKnowHow@gmail.com", "5039279423"),
+    (4, "gooseyGirl", "Goose", "gooser@gmail.com", "5039613187"),
+    (5, "stranger danger", "stranger", "danger@gmail.com", "1234567890"),
+]
 my_cursor.executemany(insert_sql, users)
 # my_db.commit() is needed to save the changes made to the db
 my_db.commit()
@@ -41,17 +93,15 @@ select_all()
 
 # update information on a few users
 update_sql = "update user set name=%s where name=%s"
-users_to_update = [('robbie', 'rob'),('elser', 'elsa')]
+users_to_update = [("Robbie", "Robert"), ("Elser", "Elsa")]
 my_cursor.executemany(update_sql, users_to_update)
 my_db.commit()
-print("updated some users")
+print("updated users names")
 select_all()
-    
+
 # delete a user
-delete_sql = "delete from user where name='stranger danger'"
+delete_sql = "delete from user where name='stranger'"
 my_cursor.execute(delete_sql)
 my_db.commit()
 print("deleted a user")
 select_all()
-
-
--- rob_wsgi.py
+++ rob_wsgi.py
@@ -2,117 +2,121 @@ import cgi, types
 from json import loads, dumps
 from copy import deepcopy
 from urllib import parse as urlparse
-from gevent import monkey; monkey.patch_all()
+from gevent import monkey
+
+monkey.patch_all()
 from gevent.pywsgi import WSGIServer
 import gevent
 from db_connection import select_all, create_user
 
+
 # what did the types get updated to for python3
 def parseAndDelistArguments(args):
-	print("Line 11: ",type(args))
-	# checking if type of args is string or unicode, and that the first character is a curly brace or square bracket
-	if type(args) == str and args[:1] in ['{', '[']:
-		args = loads(args)
-		if type(args) == list: return args;
-	else:
-		args = urlparse.parse_qs(args)
+    print("Line 11: ", type(args))
+    # checking if type of args is string or unicode, and that the first character is a curly brace or square bracket
+    if type(args) == str and args[:1] in ["{", "["]:
+        args = loads(args)
+        if type(args) == list:
+            return args
+    else:
+        args = urlparse.parse_qs(args)
 
-	return delistArguments(args)
+    return delistArguments(args)
 
 
 def delistArguments(args):
-	'''
-		Takes a dictionary, 'args' and de-lists any single-item lists then
-		returns the resulting dictionary.
-		{'foo': ['bar']} would become {'foo': 'bar'}
-	'''
-	
-	def flatten(k,v):
-		if len(v) == 1 and type(v) is list: return (str(k), v[0]);
-		return (str(k), v)
+    """
+    Takes a dictionary, 'args' and de-lists any single-item lists then
+    returns the resulting dictionary.
+    {'foo': ['bar']} would become {'foo': 'bar'}
+    """
 
-	return dict([flatten(k,v) for k,v in args.items()])
+    def flatten(k, v):
+        if len(v) == 1 and type(v) is list:
+            return (str(k), v[0])
+        return (str(k), v)
+
+    return dict([flatten(k, v) for k, v in args.items()])
 
 
 def application(env, start_response):
-	path = env['PATH_INFO']
-	print('_wsgi.path(initial):', path)
+    path = env["PATH_INFO"]
+    print("_wsgi.path(initial):", path)
 
-	if path == '/':
-		start_response('302 Moved Temporarily', [('Location', 'http://blueboard.com')])
-		return ''
+    if path == "/":
+        start_response("302 Moved Temporarily", [("Location", "http://blueboard.com")])
+        return ""
 
-	if path == '/favicon.ico': 
-		start_response('301 Moved Permanently', [('Location', 'http://blueboard.com/favicon.ico')])
-		return ''
-	
-	path = path.split('/')
-	if len(path) < 2 or path[1] != 'v1.0':
-		start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
-		return 'invalid API version'
+    if path == "/favicon.ico":
+        start_response(
+            "301 Moved Permanently", [("Location", "http://blueboard.com/favicon.ico")]
+        )
+        return ""
 
-	path = path[2:]
-	if not path[-1]: path.pop();
-	print('_wsgi.path(cleaned):', path)
+    path = path.split("/")
+    if len(path) < 2 or path[1] != "v1.0":
+        start_response("500 Internal Server Error", [("Content-Type", "text/plain")])
+        return "invalid API version"
 
-	method = env['REQUEST_METHOD'].upper()
-	print('method:', method)
-	
-	print(env['QUERY_STRING'])
-	args = parseAndDelistArguments(env['QUERY_STRING'])
-	print('args: 63 ', args)
+    path = path[2:]
+    if not path[-1]:
+        path.pop()
+    print("_wsgi.path(cleaned):", path)
 
-# env variable, check it out in docs
-	wsgi_input = env['wsgi.input']
+    method = env["REQUEST_METHOD"].upper()
+    print("method:", method)
 
-	if wsgi_input.content_length and method != 'PUT':
-		post_env = env.copy()
-		post_env['QUERY_STRING'] = ''
-		form = cgi.FieldStorage(
-			fp=env['wsgi.input'],
-			environ=post_env,
-			keep_blank_values=True
-		)
-		# list comprehension: pythonic thing, a for statement that outputs a list. Takes in data and makes a list out of it.
-		form_data = [(k, form[k].value) for k in form.keys()]
-		# what does "update" do to a dictionary?
-		args.update(form_data)
+    print(env["QUERY_STRING"])
+    args = parseAndDelistArguments(env["QUERY_STRING"])
+    print("args: 63 ", args)
 
-	if method == 'PUT':	
-		wsgi_input = wsgi_input.read()
-		args.update(parseAndDelistArguments(wsgi_input))
+    # env variable, check it out in docs
+    wsgi_input = env["wsgi.input"]
 
-	#now call the methods as needed
-	# test
-	
-	if "users" in path and method == "GET":
-		start_response('200 OK', [('Content-Type', 'text/html')])
-		response = select_all()
+    if wsgi_input.content_length and method != "PUT":
+        post_env = env.copy()
+        post_env["QUERY_STRING"] = ""
+        form = cgi.FieldStorage(
+            fp=env["wsgi.input"], environ=post_env, keep_blank_values=True
+        )
+        # list comprehension: pythonic thing, a for statement that outputs a list. Takes in data and makes a list out of it.
+        form_data = [(k, form[k].value) for k in form.keys()]
+        # what does "update" do to a dictionary?
+        args.update(form_data)
 
-	if "users" in path and method == "POST":
-		start_response('200 OK', [('Content-Type', 'text/html')])
-		response = create_user(args)
-		
+    if method == "PUT":
+        wsgi_input = wsgi_input.read()
+        args.update(parseAndDelistArguments(wsgi_input))
+
+    # now call the methods as needed
+    # test
+
+    if "users" in path and method == "GET":
+        start_response("200 OK", [("Content-Type", "text/html")])
+        response = select_all()
+
+    if "users" in path and method == "POST":
+        start_response("200 OK", [("Content-Type", "text/html")])
+        response = create_user(args)
+
+    try:
+        ret = {
+            "path": path,
+            "args": args,
+            "method": method,
+            "response": response,  # the output of the functions you call
+        }
+        start_response("200 OK", [("Content-Type", "application/json")])
+        print(dumps(ret).encode("utf-8"))
+        return [dumps(ret).encode("utf-8")]
+
+    except Exception as inst:
+        print("line: 106", inst)
+        start_response("500 Internal Server Error", [("Content-Type", "text/plain")])
+        return repr(inst)
 
 
-	try:
-		ret = { 
-			'path' : path,
-			'args' : args,
-			'method' : method,
-			'response': response #the output of the functions you call
-		}
-		start_response('200 OK', [('Content-Type', 'application/json')])
-		print(dumps(ret).encode("utf-8"))
-		return [dumps(ret).encode("utf-8")]
-
-	except Exception as inst:
-		print("line: 106", inst)
-		start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
-		return repr(inst)
-
-
-if __name__ == '__main__':
-	wsgi_port = 8888
-	print('serving on %s...' % wsgi_port)
-	WSGIServer(('', wsgi_port), application).serve_forever()
+if __name__ == "__main__":
+    wsgi_port = 8888
+    print("serving on %s..." % wsgi_port)
+    WSGIServer(("", wsgi_port), application).serve_forever()
