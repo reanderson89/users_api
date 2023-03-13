84846f553c623bf29d998ec5f6aa0547a427917b
robAnderson <reanderson89@gmail.com>
2023-03-13 16:15:20 -0700

    abstracted the cursor to the db_connection, refactored for loop in select_all()

--- db_connection.py
+++ db_connection.py
@@ -3,34 +3,50 @@ import hashlib
 import uuid
 import os
 
-host = os.environ.get("LOCAL_DB_HOST")
-user = os.environ.get("LOCAL_DB_USER")
-password = os.environ.get("LOCAL_DB_PASSWORD") 
+# get database connection info from environment variables
+host = os.environ.get("DB_HOST")
+user = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASSWORD") 
+database = os.environ.get("DATABASE")
+port = os.environ.get("DB_PORT")
 
-my_db = mysql.connector.connect(host="localhost", user="root", password="password")
+print("connecting to DB...")
 
-my_cursor = my_db.cursor()
+my_db = mysql.connector.connect(host=host, user=user, password=password, database=database, port=port)
 
+if my_db.is_connected():
+    print("connected to database")
+else:
+    print("connection to database was unsuccessful")
+
+def get_cursor():
+    return my_db.cursor()
+
+# The below code is for working locally and having a fresh database and dummy data each time you run the application
 # drop db if it exists and then create it again and establish usage
-my_cursor.execute("drop database if exists user_db;")
-my_cursor.execute("create database user_db;")
-my_cursor.execute("use user_db;")
+def create_db():
+    with get_cursor() as cur:
+        cur.execute("drop database if exists user_db;")
+        cur.execute("create database user_db;")
+        cur.execute("use user_db;")
 
-# create table
-my_cursor.execute(
-    """
-CREATE TABLE `user` (
-  `pid` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `uuid` varchar(56) DEFAULT NULL UNIQUE,
-  `username` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL UNIQUE,
-  `sms` varchar(25) DEFAULT NULL UNIQUE,
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `lastUpdated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-"""
-)
+def create_table():
+    # create table
+    with get_cursor() as cur:
+        cur.execute(
+            """
+        CREATE TABLE `user` (
+        `pid` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        `uuid` varchar(56) DEFAULT NULL UNIQUE,
+        `username` varchar(255) DEFAULT NULL,
+        `name` varchar(255) DEFAULT NULL,
+        `email` varchar(255) DEFAULT NULL UNIQUE,
+        `sms` varchar(25) DEFAULT NULL UNIQUE,
+        `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `lastUpdated` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        );
+        """
+        )
 
 
 
@@ -41,43 +57,47 @@ def generate_uuid(email):
     return str(uuid_string)
 
 def insert_dummy_data():
-    users = [
-        (
-            generate_uuid("reanderson89@gmail.com"),
-            "reanderson89",
-            "Robert",
-            "reanderson89@gmail.com",
-            "5039279423",
-        ),
-        (
-            generate_uuid("staudere@gmail.com"),
-            "estauder90",
-            "Elsa",
-            "staudere@gmail.com",
-            "5039613187",
-        ),
-        (
-            generate_uuid("missKnowHow@gmail.com"),
-            "appaKnowHow",
-            "Appa",
-            "missKnowHow@gmail.com",
-            "5039279424",
-        ),
-        (
-            generate_uuid("gooser@gmail.com"),
-            "gooseyGirl",
-            "Goose",
-            "gooser@gmail.com",
-            "5039613188",
-        ),
-    ]
-    insert_sql = """
-    INSERT INTO user (uuid, username, name, email, sms)
-    VALUES(%s, %s, %s, %s, %s);
-    """
-    my_cursor.executemany(insert_sql, users)
-    # my_db.commit() is needed to save the changes made to the db
-    my_db.commit()
-    print("adding seed data...")
+    with get_cursor() as cur:
+        users = [
+            (
+                generate_uuid("reanderson89@gmail.com"),
+                "reanderson89",
+                "Robert",
+                "reanderson89@gmail.com",
+                "5039279423",
+            ),
+            (
+                generate_uuid("staudere@gmail.com"),
+                "estauder90",
+                "Elsa",
+                "staudere@gmail.com",
+                "5039613187",
+            ),
+            (
+                generate_uuid("missKnowHow@gmail.com"),
+                "appaKnowHow",
+                "Appa",
+                "missKnowHow@gmail.com",
+                "5039279424",
+            ),
+            (
+                generate_uuid("gooser@gmail.com"),
+                "gooseyGirl",
+                "Goose",
+                "gooser@gmail.com",
+                "5039613188",
+            ),
+        ]
+        insert_sql = """
+        INSERT INTO user (uuid, username, name, email, sms)
+        VALUES(%s, %s, %s, %s, %s);
+        """
+        cur.executemany(insert_sql, users)
+        # my_db.commit() is needed to save the changes made to the db
+        my_db.commit()
+        print("adding seed data...")
 
-insert_dummy_data()
+# uncomment if you need to work locally and want a fresh database and dummy data 
+# create_db()
+# create_table()
+# insert_dummy_data()
--- orm.py
+++ orm.py
@@ -1,27 +1,26 @@
-import mysql.connector
 import hashlib
 import uuid
-import os
+from db_connection import my_db, get_cursor
 
-# get database connection info from environment variables
-host = os.environ.get("DB_HOST")
-user = os.environ.get("DB_USER")
-password = os.environ.get("DB_PASSWORD") 
-database = os.environ.get("DATABASE")
-port = os.environ.get("DB_PORT")
+# # get database connection info from environment variables
+# host = os.environ.get("DB_HOST")
+# user = os.environ.get("DB_USER")
+# password = os.environ.get("DB_PASSWORD")
+# database = os.environ.get("DATABASE")
+# port = os.environ.get("DB_PORT")
 
-print("connecting to DB...")
+# print("connecting to DB...")
 
-my_db = mysql.connector.connect(host=host, user=user, password=password, database=database, port=port)
+# my_db = mysql.connector.connect(host=host, user=user, password=password, database=database, port=port)
 
-if my_db.is_connected():
-    print("connected to database")
-else:
-    print("connection to database was unsuccessful")
+# if my_db.is_connected():
+#     print("connected to database")
+# else:
+#     print("connection to database was unsuccessful")
 
 
 def check_user_exists(uuid):
-    with my_db.cursor() as cur:
+    with get_cursor() as cur:
         sql = "SELECT uuid FROM user WHERE uuid=%s"
         cur.execute(sql, [uuid])
         result = cur.fetchone()
@@ -32,7 +31,7 @@ def check_user_exists(uuid):
 
 
 def check_email_exists(email):
-    with my_db.cursor() as cur:
+    with get_cursor() as cur:
         sql = "SELECT email FROM user WHERE email=%s"
         cur.execute(sql, [email])
         result = cur.fetchone()
@@ -43,7 +42,7 @@ def check_email_exists(email):
 
 
 def check_sms_exists(sms):
-    with my_db.cursor() as cur:
+    with get_cursor() as cur:
         sql = "SELECT sms FROM user WHERE sms=%s"
         cur.execute(sql, [sms])
         result = cur.fetchone()
@@ -62,51 +61,51 @@ def generate_uuid(email):
 
 # GET all users
 def select_all():
-    with my_db.cursor() as cur:
+    with get_cursor() as cur:
         # select all users from the table (READ)
         cur.execute("select uuid, username, name, email, sms from user;")
         # the fetchall() or fetchone() is going to fetch the information selected from the above query and store it on the result variable
         result = cur.fetchall()
         print(result)
-        list_result = []
-        for row in result:
-            list_result.append(
-                {
-                    "uuid": row[0],
-                    "username": row[1],
-                    "name": row[2],
-                    "email": row[3],
-                    "sms": row[4],
-                }
-            )
+        list_result = [
+            {
+                "uuid": row[0],
+                "username": row[1],
+                "name": row[2],
+                "email": row[3],
+                "sms": row[4],
+            }
+            for row in result
+        ]
+
         return list_result
 
 
 # GET one user by email, need to refactor to find by UUID
 def select_one_user(uuid):
-    with my_db.cursor() as cur:
+    with get_cursor() as cur:
         if check_user_exists(uuid):
             cur.execute(
-                "select uuid, username, name, email, sms from user where uuid=%s", [uuid]
+                "select uuid, username, name, email, sms from user where uuid=%s",
+                [uuid],
             )
             result = cur.fetchone()
-            listResult = [
-                {
-                    "uuid": result[0],
-                    "username": result[1],
-                    "name": result[2],
-                    "email": result[3],
-                    "sms": result[4],
-                }
-            ]
-            return listResult
+            dictResult = {
+                "uuid": result[0],
+                "username": result[1],
+                "name": result[2],
+                "email": result[3],
+                "sms": result[4],
+            }
+
+            return dictResult
         else:
             return False
 
 
 # POST a user to the database, need to figure out how to create the UUID. Check for built-in python methods.
 def create_user(args):
-    with my_db.cursor() as cur:
+    with get_cursor() as cur:
         if check_email_exists(args["email"]):
             return "email"
         if check_sms_exists(args["sms"]):
@@ -123,7 +122,7 @@ def create_user(args):
 
 # PUT, update a user in the database. Start by finding the user and getting their information to check against the incoming information.
 def update_user(args, uuid):
-    with my_db.cursor() as cur:
+    with get_cursor() as cur:
         if check_user_exists(uuid):
             user_to_update = select_one_user(uuid)[0]
             email_exists = check_email_exists(args["email"])
@@ -161,7 +160,7 @@ def update_user(args, uuid):
 
 
 def delete_user(uuid):
-    with my_db.cursor() as cur:
+    with get_cursor() as cur:
         if check_user_exists(uuid):
             delete_sql = "delete from user where uuid=%s"
             cur.execute(delete_sql, [uuid])
