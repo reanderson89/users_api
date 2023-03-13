ec21dfcd8379463f510dcb9231211c9be42970e8
robAnderson <reanderson89@gmail.com>
2023-03-12 17:36:09 -0700

    black

--- orm.py
+++ orm.py
@@ -2,6 +2,7 @@ from db_connection import my_db, my_cursor
 import hashlib
 import uuid
 
+
 def check_user_exists(uuid):
     sql = "SELECT uuid FROM user WHERE uuid=%s"
     my_cursor.execute(sql, [uuid])
@@ -10,7 +11,8 @@ def check_user_exists(uuid):
         return True
     else:
         return False
-    
+
+
 def check_email_exists(email):
     sql = "SELECT email FROM user WHERE email=%s"
     my_cursor.execute(sql, [email])
@@ -19,7 +21,8 @@ def check_email_exists(email):
         return True
     else:
         return False
-    
+
+
 def check_sms_exists(sms):
     sql = "SELECT sms FROM user WHERE sms=%s"
     my_cursor.execute(sql, [sms])
@@ -28,7 +31,8 @@ def check_sms_exists(sms):
         return True
     else:
         return False
-    
+
+
 def generate_uuid(email):
     hash_object = hashlib.sha224(email.encode())
     uuid_string = uuid.UUID(hash_object.hexdigest()[0:32])
@@ -44,30 +48,38 @@ def select_all():
     result = my_cursor.fetchall()
     dictResult = []
     for row in result:
-        dictResult.append({
-            "uuid": row[0],
-            "username": row[1],
-            "name": row[2],
-            "email": row[3],
-            "sms": row[4],
-        })
+        dictResult.append(
+            {
+                "uuid": row[0],
+                "username": row[1],
+                "name": row[2],
+                "email": row[3],
+                "sms": row[4],
+            }
+        )
     return dictResult
+
 
 # GET one user by email, need to refactor to find by UUID
 def select_one_user(uuid):
     if check_user_exists(uuid):
-        my_cursor.execute("select uuid, username, name, email, sms from user where uuid=%s", [uuid])
+        my_cursor.execute(
+            "select uuid, username, name, email, sms from user where uuid=%s", [uuid]
+        )
         result = my_cursor.fetchone()
-        listResult = [{
-            "uuid": result[0],
-            "username": result[1],
-            "name": result[2],
-            "email": result[3],
-            "sms": result[4],
-        }]
+        listResult = [
+            {
+                "uuid": result[0],
+                "username": result[1],
+                "name": result[2],
+                "email": result[3],
+                "sms": result[4],
+            }
+        ]
         return listResult
     else:
         return False
+
 
 # POST a user to the database, need to figure out how to create the UUID. Check for built-in python methods.
 def create_user(args):
@@ -84,9 +96,10 @@ def create_user(args):
     user_from_db = select_one_user(uuid)
     return user_from_db
 
+
 # PUT, update a user in the database. Start by finding the user and getting their information to check against the incoming information.
 def update_user(args, uuid):
-    if check_user_exists(uuid): 
+    if check_user_exists(uuid):
         user_to_update = select_one_user(uuid)[0]
         email_exists = check_email_exists(args["email"])
         sms_exists = check_sms_exists(args["sms"])
@@ -95,7 +108,7 @@ def update_user(args, uuid):
             return "email"
         if sms_exists and args["sms"] != user_to_update["sms"]:
             return "phone"
-        
+
         incoming_user_data = args
         print(incoming_user_data.keys())
         fields_to_update = list(incoming_user_data.keys())
@@ -104,8 +117,16 @@ def update_user(args, uuid):
                 continue
             user_to_update[field] = incoming_user_data[field]
 
-        update_sql = "UPDATE user SET username=%s, name=%s, email=%s, sms=%s WHERE uuid=%s"
-        updated_user = [user_to_update["username"], user_to_update["name"], user_to_update["email"], user_to_update["sms"], uuid]
+        update_sql = (
+            "UPDATE user SET username=%s, name=%s, email=%s, sms=%s WHERE uuid=%s"
+        )
+        updated_user = [
+            user_to_update["username"],
+            user_to_update["name"],
+            user_to_update["email"],
+            user_to_update["sms"],
+            uuid,
+        ]
         my_cursor.execute(update_sql, updated_user)
         my_db.commit()
         user_from_db = select_one_user(user_to_update["uuid"])
@@ -113,12 +134,13 @@ def update_user(args, uuid):
     else:
         return False
 
+
 def delete_user(uuid):
     if check_user_exists(uuid):
         delete_sql = "delete from user where uuid=%s"
         my_cursor.execute(delete_sql, [uuid])
         my_db.commit()
-        message = "User with uuid: "+uuid+" has been deleted."
+        message = "User with uuid: " + uuid + " has been deleted."
         return message
     else:
         return False
@@ -126,6 +148,3 @@ def delete_user(uuid):
 
 # TO DO:
 # Figure out how to make the UUID
-
-
-
